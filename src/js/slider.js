adc826021d1fec3a93f659d81e8aa1c054ac2278
blessssing <betterricherhigher@gmail.com>
2024-01-03 03:20:35 +0300

    add images, add logo icons, add slider.js, update slider product

---
+++ src/js/slider.js
@@ -0,0 +1,18 @@
+const swiper1 = new Swiper(".swiper", {
+	direction: "horizontal",
+	loop: true,
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// autoplay: {
+	// delay: 1500,
+	// },
+});
