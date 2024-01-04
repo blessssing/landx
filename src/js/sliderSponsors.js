af2361a8b89b94ea17a1515f9e49627ca73d7471
blessssing <betterricherhigher@gmail.com>
2024-01-04 15:27:35 +0300

    update images, update html sponsors, update sliderSponsors.js, update styles

--- src/js/sliderSponsors.js
+++ src/js/sliderSponsors.js
@@ -1,30 +1,12 @@
 const sliderSponsors = new Swiper(".sliderSponsors", {
-	slidesPerView: 3, // 3
+	slidesPerView: 5,
 	grid: {
 		fill: "row",
-		rows: 2, // 2
+		rows: 2,
 	},
-	spaceBetween: 10, // 30
-	// loop: true,
-	speed: 2000, // 1250
+	spaceBetween: 10,
+	speed: 2000,
 	autoplay: {
-		delay: 0,
+		delay: 1000,
 	},
-	// breakpoints: {
-	// 	// when window width is >= 320px
-	// 	320: {
-	// 		slidesPerView: 2,
-	// 		spaceBetween: 20,
-	// 	},
-	// 	// when window width is >= 480px
-	// 	480: {
-	// 		slidesPerView: 3,
-	// 		spaceBetween: 30,
-	// 	},
-	// 	// when window width is >= 640px
-	// 	640: {
-	// 		slidesPerView: 4,
-	// 		spaceBetween: 40,
-	// 	},
-	// },
 });
